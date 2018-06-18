26ae27c4865fc97cfa74e3dadc479e09866539eb
rishabh-ar <rishabh.ar@media.net>
2018-06-18 05:28:04 -0400

    Code refactor

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
-
-var entryIndex = 1; 
+var entryIndex = 1;
 var listener = new WebSocket("ws://stocks.mnet.website");
-previousTickerPrice = {}
+previousTickerPrice = {};
 
-listener.onmessage = function(event) {	
-	var input = JSON.parse(event.data);	
-	if (input.length) {
-		var arrayLength = input.length; 
+listener.onmessage = function(event) {
+ 	var input = JSON.parse(event.data);
+ 	if (input.length) {
+		var arrayLength = input.length;
 		for(var i = 0; i < arrayLength; i++) {
-			var tableBody = document.getElementById("stock");
-			var tableRow = document.createElement("tr");
-			var tableData1 = document.createElement("th");
-			tableData1.appendChild(document.createTextNode(entryIndex));
-			tableData1.setAttribute("scope", "row"); 
-			var tableData2 = document.createElement("td");
-			tableData2.appendChild(document.createTextNode(input[i][0]));
-			var tableData3 = document.createElement("td");
-			tableData3.appendChild(document.createTextNode(input[i][1])); 
-			var tableData4 = document.createElement("td");			
-			tableData4.appendChild(document.createTextNode((new Date(Date.now())).toTimeString()));
-			tableRow.appendChild(tableData1); 
-			tableRow.appendChild(tableData2); 
-			tableRow.appendChild(tableData3);
-			tableRow.appendChild(tableData4);
-			var price = parseFloat(input[i][1]);
-			if (previousTickerPrice[input[i][0]] != null) {
-				if (price < previousTickerPrice[input[i][0]]) {
-					tableRow.setAttribute("class", "table-danger"); 
-				} else {		
-					tableRow.setAttribute("class", "table-success"); 
-				}
-			}
+	    var tableBody = document.getElementById("stock");
+	    var tableRow = document.createElement("tr");
+	    var tableData1 = document.createElement("th");
+	    tableData1.appendChild(document.createTextNode(entryIndex));
+	    tableData1.setAttribute("scope", "row");
+	    var tableData2 = document.createElement("td");
+	    tableData2.appendChild(document.createTextNode(input[i][0]));
+	    var tableData3 = document.createElement("td");
+	    tableData3.appendChild(document.createTextNode(input[i][1]));
+	    var tableData4 = document.createElement("td");
+	    tableData4.appendChild(
+	    	document.createTextNode((new Date(Date.now())).toTimeString()));
+	    tableRow.appendChild(tableData1);
+	    tableRow.appendChild(tableData2);
+	    tableRow.appendChild(tableData3);
+	    tableRow.appendChild(tableData4);
+	    var price = parseFloat(input[i][1]);
+ 			if (previousTickerPrice[input[i][0]] != null) {
+  			if (price < previousTickerPrice[input[i][0]]) {
+   				tableRow.setAttribute("class", "table-danger");
+  			} else {
+   				tableRow.setAttribute("class", "table-success");
+  			}
+ 			}
 			previousTickerPrice[input[i][0]] = price;
-			tableBody.appendChild(tableRow);
-			entryIndex += 1; 
+ 			tableBody.appendChild(tableRow);
+ 			entryIndex += 1;
 		}
 	}
 }
